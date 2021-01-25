4c472496631c57851d0ac06aabaa4fa343a3a861
Anand <anand17218@iiitd.ac.in>
2021-01-25 12:38:03 +0530

    grayscale label to color

--- libs/datasets/pascalvoc.py
+++ libs/datasets/pascalvoc.py
@@ -114,7 +114,7 @@ class VOCSegmentation(data.Dataset):
             tuple: (image, target) where target is the image segmentation.
         """
         image = cv2.imread(self.images[index], cv2.IMREAD_COLOR)
-        label = cv2.imread(self.masks[index], cv2.IMREAD_COLOR)
+        label = cv2.imread(self.masks[index])
         label = voc_label_indices(label, self.colormap2label)
 
 
