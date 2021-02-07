c0922fb66d838f54c067c05c9a8654ee67a0bff0
Anand <anand17218@iiitd.ac.in>
2021-02-07 15:31:45 +0530

    changes

--- exp/test_dualseg_r_50_pascalvoc_finetrain.sh
+++ exp/test_dualseg_r_50_pascalvoc_finetrain.sh
@@ -7,5 +7,5 @@ python eval.py --data_set pascalvoc \
 --num_classes 21 \
 --arch DualSeg_res50 \
 --rgb 1 \
---restore_from "./save_dualseg_r50/pascalvoc/PSPnet.pth" \
+--restore_from "./save_dualseg_r50/pascalvoc/DualSeg_res50_final.pth" \
 --output_dir "./dual_seg_r50/pascalvoc"
