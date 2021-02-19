f6a17249efb949096013fae1fd99c77f7d909bc7
Anand <anand17218@iiitd.ac.in>
2021-02-19 11:12:27 +0530

    increasing batch size and epochs

--- exp/train_dualseg_r_50_pascalvoc_finetrain.sh
+++ exp/train_dualseg_r_50_pascalvoc_finetrain.sh
@@ -8,7 +8,7 @@ python train_distribute.py --data_set pascalvoc \
 --input_size 321 \
 --batch_size_per_gpu 10 \
 --learning_rate 0.01 \
---num_steps 500 \
+--num_steps 100 \
 --num_classes 21 \
 --save_dir "./save_dualseg_r50/pascalvoc" \
 --ohem 1 --ohem_thres 0.7 --ohem_keep 100000 \
