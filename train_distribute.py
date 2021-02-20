abe8fd0cd95ee5952cfd1ff3c5b740bae0a5d7d9
Anand <anand17218@iiitd.ac.in>
2021-02-20 08:21:01 +0530

    increasing batch size and epochs

--- train_distribute.py
+++ train_distribute.py
@@ -244,7 +244,7 @@ def main():
                       scale=args.random_scale, mirror=args.random_mirror, mean=IMG_MEAN,vars=IMG_VARS, RGB= args.rgb)
 
     classes = [label for _, label in data_set]
-    print(Counter(classes))
+    Counter(classes)
     sys.exit()
 
     trainloader = data.DataLoader(
