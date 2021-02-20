f9d510907ae819a65b8141a97b10b4f8b218335a
Anand <anand17218@iiitd.ac.in>
2021-02-20 10:45:52 +0530

    increasing batch size and epochs

--- train_distribute.py
+++ train_distribute.py
@@ -242,12 +242,11 @@ def main():
         data_set = Cityscapes(args.data_dir, args.data_list, max_iters=max_iters, crop_size=input_size,
                       scale=args.random_scale, mirror=args.random_mirror, mean=IMG_MEAN,vars=IMG_VARS, RGB= args.rgb)
 
-    instance_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    instance_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for _, label in data_set:
         for pixel in label.flatten():
-            if(int(pixel) >= len(instance_count)):
-                print(pixel)
-                sys.exit()
+            if(int(pixel) == 255):
+                pixel = 21
             instance_count[int(pixel)] += 1
     print(instance_count)
     sys.exit()
