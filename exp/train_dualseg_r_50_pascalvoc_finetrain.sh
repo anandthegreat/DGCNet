fe1202ee8c192e8af80dc600f5d1d32339cd6446
Anand <anand17218@iiitd.ac.in>
2021-02-20 19:37:47 +0530

    increasing batch size and epochs

--- exp/train_dualseg_r_50_pascalvoc_finetrain.sh
+++ exp/train_dualseg_r_50_pascalvoc_finetrain.sh
@@ -5,11 +5,10 @@
 python train_distribute.py --data_set pascalvoc \
 --data_dir "/home/anand17218/GALD-DGCNet/dataset/PASCALVOC2012/VOCdevkit/VOC2012" \
 --arch DualSeg_res50 \
---batch_size_per_gpu 10 \
+--batch_size_per_gpu 8 \
 --learning_rate 0.01 \
---num_steps 500 \
+--num_steps 5 \
 --num_classes 21 \
---input_size 321 \
 --save_dir "./save_dualseg_r50/pascalvoc" \
 --ohem 1 --ohem_thres 0.7 --ohem_keep 100000 \
 --log_file "dual_seg_r50_pascalvoc.log"
--- libs/datasets/pascalvoc.py
+++ libs/datasets/pascalvoc.py
@@ -2,6 +2,7 @@ import os
 import tarfile
 import collections
 from torch.utils import data
+from torchvision import transforms
 from PIL import Image
 import cv2
 import random
@@ -50,6 +51,13 @@ def voc_label_indices(colormap, colormap2label):
     #calculate label value and find it's label mapping from colormap2label
     return colormap2label[idx]
 
+def voc_rand_crop(feature, label, height, width):
+    """Randomly crop for both feature and label images."""
+    rect = transforms.RandomCrop.get_params(feature, (height, width))
+    feature = transforms.functional.crop(feature, *rect)
+    label = transforms.functional.crop(label, *rect)
+    return feature, label
+
 class VOCSegmentation(data.Dataset):
     """`Pascal VOC <http://host.robots.ox.ac.uk/pascal/VOC/>`_ Segmentation Dataset.
 
@@ -76,7 +84,7 @@ class VOCSegmentation(data.Dataset):
             crop_size=(320,480),
             mean=(128,128,128),
             vars=(1,1,1),
-            scale=True,
+            scale=False,
             ignore_label=255,
             image_set= "train",
             download= False,
@@ -120,6 +128,12 @@ class VOCSegmentation(data.Dataset):
         # print("first mask file name: ", self.masks[0])
         assert (len(self.images) == len(self.masks))
 
+    # As some images in the dataset may be smaller than the output dimensions specified 
+    # for random cropping, we must remove these example by using a custom filter function.
+    def filter(self, imgs):
+        return [img for img in imgs if (
+            img.shape[1] >= self.crop_size[0] and img.shape[2] >= self.crop_size[1])]
+
     def __getitem__(self, index):
         """
         Args:
@@ -131,42 +145,45 @@ class VOCSegmentation(data.Dataset):
         image = cv2.imread(self.images[index], cv2.IMREAD_COLOR)
         label = cv2.imread(self.masks[index])
         # print("LABEL'S SHAPE IS: ", label.shape)
-        label = voc_label_indices(label, self.colormap2label)
+        # label = voc_label_indices(label, self.colormap2label)
         # print("successfully loaded image and label")
-
+        image = self.filter(image)
+        label = self.filter(label)
+        image, label = voc_rand_crop(self.image, self.label, *self.crop_size)
+        return image, voc_label_indices(label, self.colormap2label)
 
         # if self.transforms is not None:
         #     image, label = self.transforms(image, label)
-        if self.scale:
-            f_scale = 0.7 + random.randint(0, 14) / 10.0
-            image=cv2.resize(image,None,fx=f_scale,fy=f_scale,interpolation=cv2.INTER_LINEAR)
-            label=cv2.resize(label,None,fx=f_scale,fy=f_scale,interpolation=cv2.INTER_NEAREST)
-        image = np.asarray(image, np.float32)
-        image -= self.mean
-        image /= self.vars
-        img_h, img_w = label.shape
-        pad_h = max(self.crop_h - img_h, 0)
-        pad_w = max(self.crop_w - img_w, 0)
-        if pad_h > 0 or pad_w > 0:
-            img_pad = cv2.copyMakeBorder(image, 0, pad_h, 0,
-                pad_w, cv2.BORDER_CONSTANT,
-                value=(0.0, 0.0, 0.0))
-            label_pad = cv2.copyMakeBorder(label, 0, pad_h, 0,
-                pad_w, cv2.BORDER_CONSTANT,
-                value=(self.ignore_label,))
-        else:
-            img_pad, label_pad = image, label
+        # if self.scale:
+        #     f_scale = 0.7 + random.randint(0, 14) / 10.0
+        #     image=cv2.resize(image,None,fx=f_scale,fy=f_scale,interpolation=cv2.INTER_LINEAR)
+        #     label=cv2.resize(label,None,fx=f_scale,fy=f_scale,interpolation=cv2.INTER_NEAREST)
+        # image = np.asarray(image, np.float32)
+        # image -= self.mean
+        # image /= self.vars
+        # img_h, img_w = label.shape
+        # pad_h = max(self.crop_h - img_h, 0)
+        # pad_w = max(self.crop_w - img_w, 0)
+        # if pad_h > 0 or pad_w > 0:
+        #     img_pad = cv2.copyMakeBorder(image, 0, pad_h, 0,
+        #         pad_w, cv2.BORDER_CONSTANT,
+        #         value=(0.0, 0.0, 0.0))
+        #     label_pad = cv2.copyMakeBorder(label, 0, pad_h, 0,
+        #         pad_w, cv2.BORDER_CONSTANT,
+        #         value=(self.ignore_label,))
+        # else:
+        #     img_pad, label_pad = image, label
 
-        img_h, img_w = label_pad.shape
-        h_off = random.randint(0, img_h - self.crop_h)
-        w_off = random.randint(0, img_w - self.crop_w)
+        # img_h, img_w = label_pad.shape
+        # h_off = random.randint(0, img_h - self.crop_h)
+        # w_off = random.randint(0, img_w - self.crop_w)
 
-        image = np.asarray(img_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
-        label = np.asarray(label_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
+        # image = np.asarray(img_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
+        # label = np.asarray(label_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
 
-        image = image.transpose((2, 0, 1))
+        # image = image.transpose((2, 0, 1))
         # print(image.shape, label.shape)
-        return image, label
+        # return image, label
 
 
     def __len__(self):
