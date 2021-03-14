8f5f26cd00b79e88d9ac2e8c8f210d98809b3056
Anand <anand17218@iiitd.ac.in>
2021-03-14 16:05:57 +0530

    changes

--- eval.py
+++ eval.py
@@ -227,7 +227,7 @@ def val():
         dataset = VOCSegmentation(args.data_dir, image_set = 'val', 
             scale = False, mean=IMG_MEAN, vars = IMG_VARS)
 
-    testloader = data.DataLoader(dataset, batch_size=8, shuffle=False, pin_memory=True)
+    testloader = data.DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
     print("testloader: " + str(len(testloader)))
 
     confusion_matrix = np.zeros((args.num_classes, args.num_classes))
